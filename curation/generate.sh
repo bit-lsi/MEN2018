dd0947c872ef4b0c0fb0660d20243526a26e4060
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-02-15 16:11:44 +0100

    Update with new version of INDRA

--- curation/generate.sh
+++ curation/generate.sh
@@ -4,4 +4,4 @@
 cat pubmeds.tsv | cut -f 1 | grep -e "^\d\+$" > pubmeds.txt
 
 # Generate INDRA statements and BEL from those PubMed identifiers
-bel-enrichment from_pmids --pmids pubmeds.txt --output curation.tsv --pickle-file statements.pkl 2> generation-log.txt
+bel-enrichment from_pmids --pmids pubmeds.txt --output curation.tsv --pickle-file statements.pkl 2> log.txt
