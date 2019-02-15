12850d202f81809a283e6634e5c899c8c0c60c94
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-02-15 16:45:04 +0100

    Add more content

--- curation/README.rst
+++ curation/README.rst
@@ -1,3 +1,3 @@
 Curation
 ========
-The ``generate.sh`` script was used to create ``pmids.txt``, ``statements.pkl``, and ``curation.tsv``. It is logged with ``generation-log.txt``.
+The ``generate.sh`` script was used to derive all resources from ``pubmeds.tsv``.
--- curation/generate.sh
+++ curation/generate.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-# Slice out just the identifiers column and skip the missing entries
-cat pubmeds.tsv | cut -f 1 | grep -e "^\d\+$" > pubmeds.txt
-
 # Generate INDRA statements and BEL from those PubMed identifiers
-bel-enrichment from_pmids --pmids pubmeds.txt --output curation.tsv --pickle-file statements.pkl 2> log.txt
+cat pubmeds.tsv | cut -f 1 | grep -e "^\d\+$" > articles/pubmeds.txt
+bel-enrichment from_pmids --pmids articles/pubmeds.txt --output articles/curation.tsv --pickle-file articles/statements.pkl 2> articles/log.txt
+
+mkdir TREM2
+bel-enrichment from_agents -a "TREM2" --output TREM2/curation.tsv --pickle-file TREM2/statements.pkl 2> TREM2/log.txt
+
+mkdir MAOB
+bel-enrichment from_agents -a "MAOB" --output MAOB/curation.tsv --pickle-file MAOB/statements.pkl 2> MAOB/log.txt
+
+mkdir IRS1
+bel-enrichment from_agents -a "IRS1" --output IRS1/curation.tsv --pickle-file IRS1/statements.pkl 2> IRS1/log.txt
+
+mkdir IGF1R
+bel-enrichment from_agents -a "IGF1R" --output IGF1R/curation.tsv --pickle-file IGF1R/statements.pkl 2> IGF1R/log.txt
